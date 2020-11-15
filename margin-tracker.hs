69b3c658412a49be35e8782e6ef32ad5d053f112
danse <f.occhipinti@gmail.com>
2020-11-15 05:54:30 +0100

    accept a default file for `items` and free text inputs

--- margin-tracker.hs
+++ margin-tracker.hs
@@ -1,19 +1,13 @@
 import Margin
 import System.Posix.Time
+import System.Posix.Types
 import System.Environment (getArgs)
 import Control.Monad (forever)
+import Text.Printf (printf)
+import Text.Read (readEither)
 
 enumerate :: [a] -> [(Int, a)]
 enumerate = zip [0..]
-
--- throws exceptions
-marginalise (items, t1, t2, l) =
-  let i = read l :: Int
-      p = items!!i
-      c = fromIntegral.fromEnum
-      secondsPerHour = 3600
-      interval = (c t2 - c t1) / secondsPerHour
-  in (interval, p)
 
 track items = do
   t1 <- epochTime
@@ -22,15 +16,33 @@ track items = do
   t2 <- epochTime
   pure (items, t1, t2, l)
  
+confirmation :: (Float, String) -> IO ()
+confirmation (i, p) = printf "added %f hours for \"%s\"\n" i p
+
+-- throws exceptionsm
+marginalise :: ([String], EpochTime, EpochTime, String) -> (Float, String)
+marginalise (items, t1, t2, l) =
+  let at = (!!)
+      e = readEither l :: Either String Int
+      p = either (const l) (at items) e
+      c = fromIntegral.fromEnum
+      secondsPerHour = 3600
+      interval = (c t2 - c t1) / secondsPerHour
+  in (interval, p)
+
+-- throws exceptions
 readItems = do
-  [fileName] <- getArgs
-  contents <- readFile fileName
+  args <- getArgs
+  contents <- readFile (d args)
   pure (lines contents)
+  where d [] = "activities"
+        d args = head args
 
 main = do
   i <- readItems
   forever (do
                m <- track i
                addToDefaultFile (marginalise m)
+               confirmation (marginalise m)
           )
  
