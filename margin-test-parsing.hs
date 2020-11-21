97e80e3184759f386656b943d89e15a922dbb359
danse <f.occhipinti@gmail.com>
2020-11-21 11:00:41 +0100

    remove unused executable

--- margin-test-parsing.hs
+++
@@ -1,9 +0,0 @@
-import Margin (parseMarginFile)
-import System.Environment (getArgs)
-
-respond = either id (const "parsing successful")
-
-main = do
-  [path] <- getArgs
-  result <- parseMarginFile path
-  print (respond result)
