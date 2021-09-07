1ac5f7504e240da0a76447c7118ac16403d96b25
danse <f.occhipinti@gmail.com>
2021-09-07 02:51:42 +0200

    improvements to the contextual tracker
    - invert the arrow to follow math intuition
    - read activities from the existing file
    - sort activities by length

--- margin-tracker-contextual.hs
+++ margin-tracker-contextual.hs
@@ -1,18 +1,16 @@
-{-
+{-# LANGUAGE RecordWildCards #-}
 
-Enter submits, Ctrl-D (or Ctrl-C) cancels
-
--}
 import Margin
 import System.Posix.Time
 import System.Posix.Types
 import System.Environment (getArgs)
-import Control.Monad (join, sequence)
+import Control.Monad (join, sequence, when)
 import System.IO.Error ()
 import Control.Exception (try)
 import Text.Read (readMaybe)
 import Safe (atMay)
-import Data.List (partition, intercalate)
+import Data.List (partition, intercalate, sortOn, nub)
+import Data.Maybe (fromMaybe)
 import Data.Set (
   Set (..),
   fromList,
@@ -23,8 +21,11 @@ import Data.Set (
 import Options.Applicative
 import Data.Semigroup ((<>))
 
-enumerate :: Set a -> [(Int, a)]
-enumerate = zip [0..] . toList
+selectionList :: Set String -> [String]
+selectionList = sortOn length . toList
+
+enumerate :: Set String -> [(Int, String)]
+enumerate = zip [0..] . selectionList
 
 data StepState = Logging | Prompt
 
@@ -45,7 +46,7 @@ data Message = Enumerate State
 showMessage :: Message -> String
 showMessage (Enumerate state) = show (enumerate (stateItems state))
                                 ++ "\n"
-                                ++ intercalate " < " (reverse (stateContext state))
+                                ++ intercalate " > " (reverse (stateContext state))
 showMessage (Paused) = "paused. enter to select an activity"
 showMessage (Added description) = "added "++description++" to margin file"
 showMessage (Elapsed h)
@@ -77,7 +78,7 @@ updateState Continue state = state { stateStep = Logging }
 updateState (Deepen userLine) state@(State items context Prompt) =
   let
     selected :: Maybe String
-    selected = readMaybe userLine >>= atMay (toList items)
+    selected = readMaybe userLine >>= atMay (selectionList items)
     selection = maybe userLine id selected
   in state {
     stateContext = selection:context,
@@ -129,26 +130,55 @@ defActivities :: [FilePath] -> [FilePath]
 defActivities [] = ["margin-tracker-activities"]
 defActivities o  = o
 
-makeItems :: Either IOError [String] -> Set String
-makeItems = fromList . either (const []) parseContents 
-  where parseContents cont = join (map lines cont)
+makeItems
+  :: Either IOError [String]
+  -> Set String
+  -> Set String
+makeItems eitherContents marginSet =
+  union contentSet marginSet
+  where
+    contentSet = fromList $ either (const []) parseContents eitherContents
+    parseContents cont = join $ lines <$> cont
 
 data Options = Options {
-  optActivities :: [String],
-  optMargin :: Maybe String
+  activityFiles :: [String],
+  marginFile :: Maybe String,
+  marginDescriptionLimit :: Maybe Int,
+  verbose :: Bool
   }
 
 options :: Parser Options
 options = Options
           <$> many (strOption (long "activities"))
           <*> optional (strOption (long "margin-data"))
+          <*> optional (option auto (long "margin-description-limit"))
+          <*> switch (long "verbose")
 
 main :: IO ()
 main = do
-  opts <- execParser (info options fullDesc)
-  let paths = defActivities (optActivities opts)
-      readItems = sequence (map readFile paths)
-    in (do
-      contents <- try readItems
-      trackingData <- step (State (makeItems contents) [] Prompt)
-      mapM_ (addToMaybeFile (optMargin opts)) trackingData)
+  Options{..} <- execParser (info options fullDesc)
+  eitherEitherMargin <- try $ parseMaybeFile marginFile
+  eitherMargin <- case eitherEitherMargin of
+    Left error -> do
+      print (error :: IOError)
+      putStrLn "no activities will be taken by the file"
+      pure $ Right []
+    Right e -> pure e
+  let paths = defActivities activityFiles
+      margin = either (const []) id eitherMargin
+      marginSet = fromList $ takeMargins
+      takeMargins = take (fromMaybe 10 marginDescriptionLimit) $ parseMargins
+      parseMargins =
+        sortOn length
+        $ nub
+        $ join
+        $ words . description
+        <$> margin
+      showMargins = do
+        putStrLn $ "parsed: " <> (show parseMargins)
+        putStrLn $ "taking: " <> (show takeMargins)
+  when verbose showMargins
+  eitherContents <- try $ sequence $ map readFile paths
+  let activities = makeItems eitherContents marginSet
+  trackingData <- step (State activities [] Prompt)
+  mapM_ (addToMaybeFile marginFile) trackingData
