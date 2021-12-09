893ae30c3bde92e3a5dbcffc7f5c6dfb603d6256
danse <f.occhipinti@gmail.com>
2021-12-09 07:32:31 +0100

    the new margin format uses comma-separated values

--- src/Margin.hs
+++ src/Margin.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Margin where
 
+import Control.Monad (when)
 import Data.Aeson(eitherDecode, FromJSON, ToJSON, eitherDecode, encode )
 import Data.ByteString.Lazy( readFile, writeFile )
-import Data.Either (rights, lefts)
+import Data.Either( rights, lefts, isRight)
 import Data.Functor( (<$>) )
 import Data.Maybe( fromMaybe )
 import Data.Semigroup
@@ -11,6 +13,12 @@ import Data.Time.Clock (UTCTime)
 import Data.Time( getCurrentTime )
 import GHC.Generics
 import System.Directory( doesFileExist )
+import Data.Csv ((.:), (.=))
+
+import qualified Data.Csv as Csv
+import qualified Data.Vector as Vector
+import qualified Data.Time.ISO8601 as ISO8601
+import qualified Data.ByteString.Char8 as Char8
 
 defaultFileName = "margin-data.json"
 
@@ -23,10 +31,45 @@ data Margin = Margin {
 instance FromJSON Margin
 instance ToJSON Margin
 
-parseMarginFile :: String -> IO (Either String [Margin])
+instance Csv.FromField UTCTime where
+  parseField packed =
+    let t = Char8.unpack packed
+    in case ISO8601.parseISO8601 t of
+      Just time -> pure time
+      Nothing -> fail (t <> " is not an ISO8601 time")
+
+instance Csv.ToField UTCTime where
+  toField = Char8.pack . ISO8601.formatISO8601Millis
+
+instance Csv.FromNamedRecord Margin where
+  parseNamedRecord r = Margin
+    <$> r .: "value"
+    <*> r .: "description"
+    <*> r .: "time"
+
+instance Csv.ToNamedRecord Margin where
+  toNamedRecord m = Csv.namedRecord [
+    "value" .= value m,
+    "description" .= description m,
+    "time" .= time m]
+
+instance Csv.DefaultOrdered Margin where
+  headerOrder _ = Csv.header ["time", "value", "description"]
+
+parseMarginFile :: FilePath -> IO (Either String [Margin])
 parseMarginFile path = do
   contents <- Data.ByteString.Lazy.readFile path
-  pure (eitherDecode contents)
+  case Csv.decodeByName contents of
+    Right (_, margins) -> pure $ Right $ Vector.toList margins
+    Left err -> do
+      putStrLn $ err <> ", trying old JSON format"
+      let eitherDecoded = eitherDecode contents
+      when (isRight eitherDecoded) (putStrLn "this JSON margin format is deprecated")
+      pure eitherDecoded
+
+serialiseMarginFile :: [Margin] -> FilePath -> IO ()
+serialiseMarginFile margins path =
+  Data.ByteString.Lazy.writeFile path (Csv.encodeDefaultOrderedByName margins)
 
 getAllMargins :: [String] -> IO [Margin]
 getAllMargins paths = do
@@ -44,22 +87,25 @@ onAllMargins paths fun = do
 marginNow :: Float -> String -> IO Margin
 marginNow value description = Margin value description <$> getCurrentTime
 
-applyToFile :: (FromJSON a, ToJSON a) => FilePath -> (Maybe a -> a) -> IO (Either String a)
-applyToFile path f = do
+applyToMargin
+  :: FilePath
+  -> (Maybe [Margin] -> [Margin])
+  -> IO (Either String [Margin])
+applyToMargin path f = do
   exists <- doesFileExist path
   if exists
     then
     do
-      string <- Data.ByteString.Lazy.readFile path
-      case eitherDecode string of
+      eitherParsed <- parseMarginFile path
+      case eitherParsed of
         Left error -> pure (Left error)
         Right decoded -> do
-          Data.ByteString.Lazy.writeFile path (encode processed)
+          serialiseMarginFile processed path
           pure (Right processed)
             where processed = f (Just decoded)
     else
     do
-      Data.ByteString.Lazy.writeFile path (encode processed)
+      serialiseMarginFile processed path
       pure (Right processed)
         where processed = f Nothing
 
@@ -69,7 +115,7 @@ addMargin newMargin maybeMargins = margins ++ [newMargin]
 
 addToFile fileName (value, description) = do
   newMargin <- marginNow value description
-  applyToFile fileName (addMargin newMargin)
+  applyToMargin fileName (addMargin newMargin)
   pure ()
 
 addToDefaultFile = addToFile ("./" ++ defaultFileName)
