0351d6934a3df1dc88b636d9241c56745eb4eebf
danse <f.occhipinti@gmail.com>
2021-12-12 06:00:33 +0100

    update `margin-stats` to the new margin format and lint the file

--- margin-stats.hs
+++ margin-stats.hs
@@ -1,5 +1,5 @@
 import Data.ByteString.Lazy( readFile )
-import Margin( value, defaultFileName, Margin )
+import Margin
 import Data.Aeson( eitherDecode )
 import Control.Applicative( (<$>) )
 import Control.Monad( void )
@@ -8,18 +8,18 @@ import System.Environment( getArgs )
 analyse :: [Margin] -> (Float, Float)
 analyse m = (s, a)
   where s = sum (map value m)
-        a = s / (fromIntegral (length m))
+        a = s / fromIntegral (length m)
 
 format :: (Float, Float) -> String
-format (s, a) = "sum: " ++ (show s) ++ ", average: " ++ (show a)
+format (s, a) = "sum: " ++ show s ++ ", average: " ++ show a
 
 showStats fileName = do
   putStrLn ("reading file " ++ fileName)
-  eitherDecoded <- eitherDecode <$> Data.ByteString.Lazy.readFile fileName
+  eitherDecoded <- parseMarginFile fileName
   putStrLn $ either id (format . analyse) eitherDecoded
 
 main = do
   args <- getArgs
-  if (null args)
+  if null args
     then showStats ("./" <> defaultFileName)
-    else void (sequence (map showStats args))
+    else mapM_ showStats args
